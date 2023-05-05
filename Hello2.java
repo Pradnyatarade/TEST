876c3c3befc25173d5077e389e80da2bb151ecc9
pradnyataradeuk <132214205+pradnyataradeuk@users.noreply.github.com>
2023-05-05 12:06:26 +0100

    Update Hello2.java

--- Hello2.java
+++ Hello2.java
@@ -4,6 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome, All!");
         System.out.println("Welcome, To Devops!");
+        System.out.println("Welcome, To New Technology!");
        
     }
 }
